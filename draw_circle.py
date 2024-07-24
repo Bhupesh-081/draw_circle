46ceaf1e2e0e2baf2ecf9b233db6c72f567edc42
Bhupesh Nandardhane <nandardhanebhupesh@gmail.com>
2024-07-25 00:03:57 +0530

    Update draw_circle.py

--- draw_circle.py
+++ draw_circle.py
@@ -6,7 +6,7 @@ class DrawCircleNode(Node):
     def __init__(self):
         super().__init__('draw_circle')
         self.publisher_ = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
-        timer_period = 0.1  # seconds
+        timer_period = 0.1 
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.get_logger().info('Draw Circle Node has been started.')
 
