6acf1301bc4e0b1d746a96d8a6cc688d1da09f74
Bhupesh Nandardhane <nandardhanebhupesh@gmail.com>
2024-07-24 23:57:55 +0530

    first commit

---
+++ draw_circle.py
@@ -0,0 +1,24 @@
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+
+class DrawCircleNode(Node):
+    def __init__(self):
+        super().__init__('draw_circle')
+        self.publisher_ = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
+        timer_period = 0.1  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.get_logger().info('Draw Circle Node has been started.')
+
+    def timer_callback(self):
+        vel_msg = Twist()
+        vel_msg.linear.x = 2.0  
+        vel_msg.angular.z = 1.0 
+        self.publisher_.publish(vel_msg)
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = DrawCircleNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
